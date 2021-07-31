d693f4e3dbcbdf1daec34c04545487495b0802eb
dairsaber <dairsaber@outlook.com>
2021-07-31 20:22:25 +0800

    feat: remove createTask function code to repository class

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -55,14 +55,7 @@ export class TasksService {
   //   return targetTask;
   // }
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-    const task = new Task();
-    task.title = title;
-    task.description = description;
-    task.status = TaskStatus.OPEN;
-    await task.save();
-
-    return task;
+    return this.taskRepository.createTask(createTaskDto);
   }
   // createTask(createTaskDto: CreateTaskDto): Task {
   //   const { title, description } = createTaskDto;
