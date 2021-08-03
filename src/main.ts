b6b4c5d15b14b0ab479ce93796c1460b1245ef4a
dairsaber <dairsaber@outlook.com>
2021-08-03 20:57:50 +0800

    feat: add configuration to app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const port = 3000;
   const app = await NestFactory.create(AppModule);
+
+  const serverConfig = config.get('server');
+  console.log(`serverConfig`, serverConfig);
 
   await app.listen(port);
   logger.log(`Application listening on port ${3000}`);
